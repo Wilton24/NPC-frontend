3696afebddf80a7036b0b6b3660379d8dc484f7d
Wilton Batiquin <cbasher24@gmail.com>
2026-01-09 19:57:05 +0800

    Refactor: Update HeaderLinks to use NavLink for player navigation

--- src/components/UI/HeaderLinks.tsx
+++ src/components/UI/HeaderLinks.tsx
@@ -1,11 +1,24 @@
+import { NavLink } from "react-router"
+
+
 export default function HeaderLinks({ title, dropdown = [] }: { title: string, dropdown: string[] }) {
+
     return (
         <li>
             <a href="#about-us">{title}</a>
             <ul className="dropdown">
                 {dropdown.map((text: string, id: number) => {
                     return (
-                        <li key={id}><a href={text}>{text}</a></li>
+                        // <NavLink to={dropdown} key={id}><a href={text}>{text}</a></NavLink>
+                        <NavLink
+                            key={id}
+                            to={`/players/${text.toLowerCase().replace(/\s+/g, '-')}`}
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            {text}
+                        </NavLink>
                     )
                 })}
             </ul>
